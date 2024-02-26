def2f157311f5cd753b22549ada17340c9a24202
antoniovmach1 <avarfer123@g.educaand.es>
2024-02-26 10:21:18 +0100

    implementacion de comando de enviar correo

--- src/Service/MessageGenerator.php
+++ src/Service/MessageGenerator.php
@@ -5,10 +5,25 @@ namespace App\Service;
 
 use Psr\Log\LoggerInterface;
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+
 class MessageGenerator
 {
     public function __construct(
         private LoggerInterface $logger,
+       
+        private MailerInterface $mailer,
+        private EntityManagerInterface $entityManager,
+
+
     ) {
     }
 
@@ -19,5 +34,27 @@ class MessageGenerator
 
         // Return a string value here
         return 'A happy message!';
+    }
+
+    public function getMensaje(): string
+    {
+        $this->logger->info('Importante');
+        // ... your logic to generate a happy message
+
+        // Return a string value here
+        return 'Hola esto funciona!';
+    }
+
+    public function sendEmail($correo, $subject, $text): bool
+    {
+        $email = (new Email())
+        ->from("freetour@gmail.com")
+        ->to($correo)
+        ->subject($subject)
+        ->text($text);
+
+        $this->mailer->send($email);
+
+        return false;
     }
 }
